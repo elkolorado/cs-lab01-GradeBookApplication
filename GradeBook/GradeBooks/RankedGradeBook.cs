b56481a2476e0519904628505700e53a1697ce89
elkolorado <vuuyadesign@gmail.com>
2023-04-03 21:14:47 +0200

    feat: Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +29,18 @@ namespace GradeBook.GradeBooks
                  : averageGrade >= gradeList[(int)Math.Ceiling(Students.Count * 0.6) - 1] ? 'C'
                  : averageGrade >= gradeList[(int)Math.Ceiling(Students.Count * 0.8) - 1] ? 'D'
                  : 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
         }
     }
 }
